864d71c17253d91ed6efe966158c50149ee53b01
Rocaleta15lehsiM@outlook.com <Rocaleta15lehsiM@outlook.com>
2022-10-28 01:01:30 -0500

    Incluyo LocalStorage asi como agrego funcion al boton agregar y eliminar tanto visual como en BD

--- app.js
+++ app.js
@@ -1,44 +1,64 @@
 
 const nombreUI = document.getElementById('nombre');
 const sueldoUI = document.getElementById('sueldo');
-const btnAgregar = document.getElementById('button');
+const btnAgregar = document.getElementById('btn');
+//const btnActualizar = document.getElementById('btn2');
 const divRead = document.getElementById('read');
-const nomina=[];
 
 btnAgregar.addEventListener('click',agregar);
+//btnActualizar.addEventListener('click',actualizar);
 
 function agregar (e){
+    
     e.preventDefault();
 
     if(nombreUI.value === '' || sueldoUI.value === ''){
+
         return;
     }
-
-    objCreado = {
+    
+    let objCreado = {
         nombre: nombreUI.value,
-        sueldo: sueldoUI.value
+        sueldo: sueldoUI.value,
+        id: Date.now()
     };
-    nomina.push(objCreado);
+
+    let arreglo = JSON.parse(localStorage.getItem("DB"));
+
+    if (arreglo === null) {
+    arreglo = [];
+    arreglo.push(objCreado);
+    localStorage.setItem("DB", JSON.stringify(arreglo));
+    } else {
+    arreglo.push(objCreado);
+    localStorage.setItem("DB", JSON.stringify(arreglo));
+    }
+
+    mostrar();
 
     nombreUI.value = '';
     sueldoUI.value = '';
-
-    mostrar();
 }
 
 function mostrar(){
+
+    let datos = JSON.parse(localStorage.getItem("DB"));
+
+    if(datos !=null){
     
-    divRead.innerHTML='';
+    divRead.innerHTML="";
     
-    nomina.forEach((element)=>{
+    datos.forEach((element)=>{
 
         let divLeer = document.createElement('div');
-        
+        divLeer.setAttribute('key',element.id);
+
         let nombre = document.createElement('h3');
         let texto = document.createTextNode(element.nombre);
         let contenedor = document.createElement('p');
         let sueldo = document.createTextNode(element.sueldo);
         let btnEliminar = document.createElement('button');
+        btnEliminar.addEventListener('click',eliminar);
         let btnEditar = document.createElement('button');
         let textobtnEliminar = document.createTextNode('Eliminar');
         let textobtnEditar = document.createTextNode('Editar');
@@ -52,9 +72,25 @@ function mostrar(){
         divLeer.appendChild(contenedor);
         divLeer.appendChild(btnEditar);
         divLeer.appendChild(btnEliminar);
-        
+
         divRead.appendChild(divLeer);
 
     });
 }
+}
+function eliminar(e){
 
+    let buscarElemento = e.path[1].getAttribute('key');
+    
+    let datos = JSON.parse(localStorage.getItem("DB"));
+    let index = datos.findIndex(element=> element.id == buscarElemento);
+
+    datos.splice(index,1);
+
+    localStorage.setItem("DB", JSON.stringify(datos));
+
+    mostrar();
+
+    nombreUI.value = '';
+    sueldoUI.value = '';
+}
